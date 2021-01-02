828eaefa04de7788f1c24f2e02a2e3fb84d11f09
Mario <e1425172@student.tuwien.ac.at>
2021-01-02 18:51:19 +0100

    remove users route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const imageService = require("../imageService/imageFromFilesystem.js")
 
 router.get('/', function (req, res, next) {
     imageService.readRandomFromFile().then(image => {
-        res.render('index', {title: 'Express', imgBase64: image});
+        res.render('index', {title: 'Digital Picture Frame', imgBase64: image});
     });
 });
 
